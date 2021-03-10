eade97bdfead2fecce982935cfc103325d6e61a3
Chaoses Ib <Chaos-es@outlook.com>
2021-03-10 22:43:05 +0800

    Add SizeColByEverything

---
+++ SizeColByEverything/README.zh-Hans.md
@@ -0,0 +1,28 @@
+# SizeColByEvetyhing
+作者：混沌Ib  
+版本：210310  
+Git：https://github.com/Chaoses-Ib/IbDOpusScripts  
+
+## 功能
+为 DO 添加一个 Size 列，通过 Everything 获取文件和文件夹的大小。
+![Preview](Preview.png)
+
+## 已知问题
+* 性能较差，3000 个文件需要一分钟多才能显示完毕（受 DO 局限）
+* 不支持符号链接和 Junction
+	* 无法在链接目录内显示文件大小
+	* 子文件夹是链接目录的话不会计入总大小（受 Ev 局限）
+* 文件数量过多时可能会引发崩溃
+
+建议只用作偶尔分析硬盘占用的工具。
+
+## 安装步骤
+1. 设置 Everything 索引文件夹大小：  
+工具-选项-索引-索引文件夹大小
+1. 安装 Everything IPC 组件  
+把 IPC 目录里的 DLL 随便找个地方放着，脚本能调用到就可以
+1. 安装 DynamicWrapperX  
+把 DynamicWrapperX 目录里的 DLL 随便找个地方放着，然后用管理员权限在那里打开命令行，执行 regsvr32 dynwrapx64.dll（或者 regsvr32 dynwrapx32.dll）
+1. 安装脚本  
+DOpus：配置-工具栏-脚本-导入，把 SizeColByEverything.js 拖进去。  
+点击编辑，把第10行的路径替换为你在第二步把 DLL 放置到的路径，注意反斜杠要写成 \\\\。
---
+++ SizeColByEverything/SizeColByEvetyhing.js
@@ -0,0 +1,65 @@
+//SizeColByEvetyhing
+//Description: Add a size column which retrieves sizes of files and folders from Everything.
+//Author: Chaoses Ib
+//Version: 210310
+//Git: https://github.com/Chaoses-Ib/IbDOpusScripts
+
+var cacheQuery = "";
+var cacheMap = DOpus.Create().Map();
+
+var path = "C:\\Program Files\\Everything\\IPC\\Everything64.dll"
+var DWX = new ActiveXObject("DynamicWrapperX");
+DWX.Register(path, "Everything_SetSearchW", "i=w");
+DWX.Register(path, "Everything_SetRequestFlags", "i=u");
+DWX.Register(path, "Everything_QueryW", "i=l", "r=l");
+DWX.Register(path, "Everything_GetNumResults", "r=u");
+DWX.Register(path, "Everything_GetResultSize", "i=up", "r=l");
+DWX.Register(path, "Everything_GetResultFileNameW", "i=u", "r=w");
+//DWX.Register(path, "Everything_SetSort", "i=u");
+//DWX.Register(path, "Everything_GetLastError", "r=u");
+
+function OnInit(initData)
+{
+	initData.name = "SizeColFromEverything";
+	initData.version = "1.0";
+	initData.copyright = "混沌 Ib";
+	initData.desc = "";
+	initData.default_enable = true;
+	initData.min_version = "12.0";
+	
+	var col = initData.AddColumn();
+	col.name = "Size_ev";
+	col.method = "OnSize_ev";
+	col.label = "Size (ev)";
+	col.justify = "left";
+	col.autogroup = true;
+	col.type = "size";
+}
+
+function OnSize_ev(scriptColData)
+{
+	var tabPath = String(scriptColData.tab.path);
+	if(cacheQuery != tabPath) evQuery(tabPath);
+	if(cacheMap.exists(scriptColData.item.name))
+		scriptColData.value = cacheMap(scriptColData.item.name);
+}
+
+function evQuery(tabPath){
+		DOpus.Output("Init");
+	cacheMap.clear(); //最短存活
+	DWX.Everything_SetSearchW('infolder:"' + tabPath +'"');
+		//DOpus.Output('infolder:"' + tabPath +'"')
+	DWX.Everything_SetRequestFlags(1+16);
+	if(!DWX.Everything_QueryW(1)) return;
+	
+	var num = DWX.Everything_GetNumResults();
+		//DOpus.Output(DWX.Everything_GetLastError() + ", " + num);
+	var size_addr = DWX.MemAlloc(16); //最短存活
+	for(var i=0; i<num; i++){
+		var name = DWX.Everything_GetResultFileNameW(i);
+		DWX.Everything_GetResultSize(i, size_addr);
+		cacheMap(name) = DWX.NumGet(size_addr, 0, "q");
+	}
+	DWX.MemFree(size_addr);
+	cacheQuery = tabPath;
+}
