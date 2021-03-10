22a383de8400a433a66e873963f17b747a220a63
Chaoses Ib <Chaos-es@outlook.com>
2021-03-10 22:00:04 +0800

    Add AutoExtract.js

---
+++ AutoExtract.js
@@ -0,0 +1,28 @@
+@script jscript
+//AutoExtract
+//Description: Extract selected archive to subfolder if there's more than one file under the root path, otherwise (only one file) extract it directly.
+//Author: Chaoses Ib
+//Version: 210310
+//Git: https://github.com/Chaoses-Ib/IbDOpusScripts
+function OnClick(clickData)
+{
+	var tab = clickData.func.sourcetab;
+	var fsutil = DOpus.FSUtil;
+	var cmd = clickData.func.command;
+	for (var eSel = new Enumerator(tab.selected); !eSel.atEnd(); eSel.moveNext())
+	{
+		var zipEnum = fsutil.ReadDir(eSel.item().RealPath);
+
+		//There's no count field
+		var count = 0;
+		for(; !zipEnum.complete; zipEnum.Next)
+			count++;
+		//DOpus.Output(count);
+
+		if(count < 2)
+			cmd.RunCommand("Copy EXTRACT HERE");
+		else
+			cmd.RunCommand("Copy EXTRACT=sub HERE");
+	}
+	return;
+}
