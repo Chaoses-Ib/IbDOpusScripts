8eebf5d6fd08dfcb3687de9681b72c7b7a58daf9
Chaoses Ib <Chaos-es@outlook.com>
2021-07-29 00:43:44 +0800

    EventViewers: Add OnViewerEvent.js

---
+++ EventViewers/OnViewerEvent.js
@@ -0,0 +1,14 @@
+function OnViewerEvent(viewerEventData){
+    info = "OnViewerEvent: " + viewerEventData.event
+    if (viewerEventData.viewer.current)
+        info += ", " + viewerEventData.viewer.current.name
+    
+    if (viewerEventData.event == "load"){
+        //info += ", " + viewerEventData.item.name  //the same as viewerEventData.viewer.current.name
+        info += ", " + (viewerEventData.viewer.index + 1) + "/" + viewerEventData.viewer.files.count
+    }
+    else if (viewerEventData.event == "click" || viewerEventData.event == "dblclk" || viewerEventData.event == "mclick")
+        info += ", (" + viewerEventData.x + ", " + viewerEventData.y + ", " + viewerEventData.w + ", " + viewerEventData.h + ")"
+    
+    DOpus.Output(info)
+}
